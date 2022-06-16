3dee3096c1114c38a7788ef39b535a75464aaf34
csiMaximSyromolotov <MaksimSyromoltov@coherentsolutions.com>
2022-06-16 14:11:23 +0300

    share option added

--- app/src/main/java/com/symaxd/qrcode/aquier/ScanResultsScreenActivity.kt
+++ app/src/main/java/com/symaxd/qrcode/aquier/ScanResultsScreenActivity.kt
@@ -1,9 +1,18 @@
 package com.symaxd.qrcode.aquier
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.core.graphics.drawable.toBitmap
 import com.symaxd.qrcode.aquier.Constants.Companion.QR_CODE_DECODED_RESULT
 import com.symaxd.qrcode.aquier.databinding.ActivityScanResultsScreenBinding
+import java.io.File
+
 
 /**Activity to show scan results*/
 class ScanResultsScreenActivity : AppCompatActivity() {
@@ -13,7 +22,37 @@ class ScanResultsScreenActivity : AppCompatActivity() {
         binding = ActivityScanResultsScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val resultText = intent?.extras?.getString(QR_CODE_DECODED_RESULT) ?: ""
-        binding.qrResultImageView.setBitmapQrCode(this, resultText)
+        with(binding.qrResultImageView) {
+            setBitmapQrCode(this@ScanResultsScreenActivity, resultText)
+            setOnClickListener {
+                sharePalette(it.background.toBitmap())
+            }
+        }
         binding.resultsText.text = resultText
+    }
+
+    private fun sharePalette(bitmap: Bitmap) {
+        val bitmapUri: Uri = getTmpFileUri(bitmap)
+        Log.i("ewg",bitmapUri.toString())
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "image/png"
+        intent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
+        startActivity(Intent.createChooser(intent, "Share Image"))
+    }
+
+    private fun getTmpFileUri(bitmap:Bitmap): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".png").apply {
+            createNewFile()
+            this.outputStream().use {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+            }
+            deleteOnExit()
+        }
+
+        return FileProvider.getUriForFile(
+            applicationContext,
+            "${BuildConfig.APPLICATION_ID}.provider",
+            tmpFile
+        )
     }
 }
